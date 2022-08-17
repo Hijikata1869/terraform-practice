e2d551e3f8929fbca976b590558b8b31748b3883
Hijikata1869 <liv.knk@gmail.com>
2022-08-17 21:48:01 +0900

    パラメータストアに関する設定をterraformで実装

--- main.tf
+++ main.tf
@@ -510,3 +510,28 @@ resource "aws_kms_alias" "example" {
   name = "alias/example"
   target_key_id = aws_kms_key.example.key_id
 }
+
+resource "aws_ssm_parameter" "db_username" {
+  name = "/db/username"
+  value = "root"
+  type = "String"
+  description = "データベースのユーザー名"
+}
+
+resource "aws_ssm_parameter" "db_raw_password" {
+  name = "/db/raw_password"
+  value = "VeryStrongPassword!"
+  type = "SecureString"
+  description = "データベースのパスワード"
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name = "db/password"
+  value = "uninitialized"
+  type = "SecureString"
+  description = "データベースのパスワード"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
