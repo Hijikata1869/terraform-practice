f306e66117e159ee081a04f42930cf5859986364
Hijikata1869 <liv.knk@gmail.com>
2022-08-22 18:10:32 +0900

    updated main.tf

--- main.tf
+++ main.tf
@@ -628,5 +628,13 @@ resource "aws_elasticache_replication_group" "example" {
   engine_version = "5.0.4"
   number_cache_clusters = 3
   node_type = "cache.m3.medium"
-  snapshot_window = "09
+  snapshot_window = "09-10:10:10"
+  snapshot_retention_limit = 7
+  maintenance_window = "mon:10:40-mon:11:40"
+  automatic_failover_enabled = true
+  port = 6379
+  apply_immediately = false
+  security_group_ids = [module.redis_sg.security_group_id]
+  parameter_group_name = aws_elasticache_parameter_group.example.name
+  subnet_group_name = aws_elasticache_subnet_group.example.name
 }
