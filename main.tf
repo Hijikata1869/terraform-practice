ab8860d7f83984a54a1f5723f2ca9735cfb02164
Hijikata1869 <liv.knk@gmail.com>
2022-08-25 20:28:09 +0900

    ロギングの設定をterraformで実装

--- main.tf
+++ main.tf
@@ -939,7 +939,7 @@ resource "aws_cloudwatch_log_group" "operation" {
   retention_in_days = 180
 }
 
-resource "aws_ssm_document" "session_manager_run_shell" [
+resource "aws_ssm_document" "session_manager_run_shell" {
   name = "SSM-SessionManagerRunShell"
   document_type = "Session"
   document_format = "JSON"
@@ -955,4 +955,83 @@ resource "aws_ssm_document" "session_manager_run_shell" [
     }
   }
 EOF
-]
+}
+
+resoruce "aws_s3_bucket" "cloudwatch_logs" {
+  bucket = "cloudwatch-logs-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "kinesis_data_firehose" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}",
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}/*",
+    ]
+  }
+}
+
+module "kinesis_data_firehose_role" {
+  source = "./iam_role"
+  name = "kinesis-data-fierhose"
+  identifier = "firehose.amazonaws.com"
+  policy = data.aws_iam_policy_document.kinesis_data_firehose.json
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "example" {
+  name = "example"
+  destination = "s3"
+
+  s3_configuration {
+    role_arn = module.kinesis_data_firehose_role.iam_role_arn
+    bucket_arn = aws_s3_bucket.cloudwatch_logs.arn
+    prefix = "ecs-scheduled-tasks/example/"
+  }
+}
+
+data "aws_iam_policy_document" "cloudwatch_logs" {
+  statement {
+    effect = "Allow"
+    actions = ["firehose:*"]
+    resources = ["arn:aws:firehose:ap-northeast-1:*:*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = ["iam:PassRole"]
+    resources = ["arn:aws:iam::*role/cloudwatch-logs"]
+  }
+}
+
+module "cloudwatch_logs_role" {
+  source = "./iam_role"
+  name = "cloudwatch-logs"
+  identifier = "logs.ap-northeast-1.amazonaws.com"
+  policy = data.aws_iam_policy_document.cloudwatch_logs.json
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "example" {
+  name = "example"
+  log_group_name = aws_cloudwatch_log_group.for_ecs_sheduled_tasks.name
+  destination_arn = aws_kinesis_firehose_delivery_stream.example.arn
+  filter_pattern = "[]"
+  role_arn = module.cloudwatch_logs_role.iam_role_arn
+}
